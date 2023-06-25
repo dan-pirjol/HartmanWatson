7aabd922ca1f4fb07a68dd7a9783075e728c74a7
dan-pirjol <60016102+dan-pirjol@users.noreply.github.com>
2023-06-25 16:53:08 -0400

    Update HWexample.R
    Added sample code comparing the exact evaluation with series expansions.

--- HWexample.R
+++ HWexample.R
@@ -6,7 +6,7 @@ x <- seq(0, 5, 0.01)
 n <- length(x)
 
 d <- c()
-for (i in 1:n) d <- c(d,Ffunc(x[i]))
+for (i in 1:n) d <- c(d,Fexp(x[i]))
 
 plot(d~x, type="l", col="blue", main="F(rho)", ylim=c(0,12))
 
@@ -14,10 +14,10 @@ plot(d~x, type="l", col="blue", main="F(rho)", ylim=c(0,12))
 #------------------------------------------------------
 # Plot the function G(rho)
 d <- c()
-for (i in 1:n) d <- c(d,Gfunc(x[i]))
+for (i in 1:n) d <- c(d,Gexp(x[i]))
 
 plot(d~x, type="l", col="blue", main="G(rho)")
-#-------------------------------------------
+#------------------------------------------------------
 
 # plot the function theta(r,t)
 # Reproduce Figure 4 in the paper
@@ -37,5 +37,47 @@ plot(y1~x, type="l", col="black", main="thetaHW(r,t)", ylim=c(0,2.5))
 lines(y2~x, type="l", col="blue")
 lines(y3~x, type="l", col="red")
 
-head(y1)
+#---------------------------------------------------------
+# compare the series expansion with the exact evaluations
 
+x <- seq(0.5, 2.0, 0.01)
+n <- length(x)
+
+f1 <- c()
+f2 <- c()
+
+for (k in 1:n) {
+  xarg <- x[k]
+  f1 <- c(f1, Finterp(xarg))
+  f2 <- c(f2, Fexp(xarg,5))
+}
+                     
+f3 <- f1 - f2
+
+#fd <- data.frame("rho"=x, "exact"=f1, "series"=f2, "diff"=f3, "log"=log(abs(f3),10))
+#fd
+
+plot(log(abs(f3),10) ~ x, type="l", main="F(rho) log-error (n=5,6,7)", xlab="rho",ylim=c(-16,0))
+lines(log(abs(f3),10) ~ x, type="l", col="blue")
+lines(log(abs(f3),10) ~ x, type="l", col="red")
+
+#----------------------------------------------------------
+g1 <- c()
+g2 <- c()
+
+for (k in 1:n) {
+  xarg <- x[k]
+  g1 <- c(g1, Ginterp(xarg))
+  g2 <- c(g2, Gexp(xarg,5))
+}
+
+g3 <- g2 - g1
+
+gd <- data.frame("rho"=x, "exact"=g1, "series"=g2, "diff"=g3)
+gd
+
+plot(log(abs(g3),10) ~ x, type="l", main="G(rho) log-error", xlab="rho")
+lines(log(abs(g3),10) ~ x, type="l", col="blue")
+lines(log(abs(g3),10) ~ x, type="l", col="red")
+
+#----------------------------------------------------------
